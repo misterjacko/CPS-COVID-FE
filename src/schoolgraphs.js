17460ba4800b4e21070293c215d408ddbf2db1e9
Jakob Ondrey <ondrey@gmail.com>
2022-04-03 13:30:56 -0500

    adds school graph and vax stuff

--- src/schoolgraphs.js
+++ src/schoolgraphs.js
@@ -1,9 +1,32 @@
-function processSchool(){
+function processSchool(schoolName){
+    getDailyTotals(schoolName);
+}
+
+function getSchoolName() {
     var parameters = location.search.substring(1).split("&");
     var temp = parameters[0].split("=");
     var schoolName =  temp[1].replaceAll("_", " ");
-    getDailyTotals(schoolName);
     return schoolName;
+}
+
+function getSpanLength(){
+    var parameters = location.search.substring(1).split("&");
+    if (parameters.length == 4) {
+        return parameters[3].split("=")[1]
+    } else {
+        return 0
+    }
+}
+
+function changeSpan(n) {
+    url = location.href.split("?")[0];
+ 
+    parameters = location.search.substring(1).split("&");
+    if (parameters.length == 4){
+        parameters.pop()
+    }
+    parameters = parameters.join("&")
+    window.location.href = url + "?" + parameters + "&span=" + n;
 }
 
 // total Cases
@@ -31,24 +54,74 @@ var dailyCase = d3.select("#dailyCasesViz")
     .append("g")
         .attr("transform","translate(" + dailyMargin.left + "," + dailyMargin.top + ")");
 
+var schoolInfo = d3.select("#schoolInfo");
 var lastCaseReported = d3.select("#lastCaseReported");
 var total7Day = d3.select("#total7Day");
 var total14Day = d3.select("#total14Day");
+var grandTotalCount = d3.select("#grandTotal");
 
 function getDailyTotals(schoolName) {
+    // d3.csv("https://cpscovid.com/data/newFormatTest.csv",
     d3.csv("./data/newFormatTest.csv",
         function(data) {            
             var dailyTotals = [];
-            var total = parseInt(data[6][schoolName], 10);
-            for (var i = data.length -1; i > 7; i--) {
+            var total = parseInt(data[9][schoolName], 10);
+            var distance = data.length -1
+            graphSpan = getSpanLength()
+            if (graphSpan > distance-10){
+                graphSpan = 0
+            }
+
+            switch(graphSpan) {
+                case 0:
+                case "0":
+                case "sy":
+                    caseSpan = distance-10;
+                    break;
+                default:
+                    caseSpan = graphSpan;
+                    break;
+
+            }
+
+            var j=0;
+            for (var i = distance; j < caseSpan; i--) {
+                if (graphSpan == "sy" && data[i].School == "20210828"){
+                    break
+                }
                 var date = d3.timeParse("%Y%m%d")(data[i].School);
                 cases = data[i][schoolName];
                 dailyTotals.push({"date": date, "cases": cases, "total": total});
                 total -= cases;
+                j++;
             };
+            // get sy total
+            total = parseInt(data[9][schoolName], 10);
+            var syStartCase = total;
+            for (var i = distance; i > 0; i--) {
+                if (data[i].School == "20210828") {
+                    break
+                };
+                syStartCase -= data[i][schoolName];
+        }   ;
 
             // add school summary data:
             var formatDate = d3.timeFormat("%a %b %-d");
+
+            // Add School Info
+            var studentCount = data[3][schoolName]
+            var vaxComplete = Math.round((data[4][schoolName]/studentCount) *1000)/10
+            var vaxFirst = Math.round((data[5][schoolName]/studentCount) *1000) / 10
+            var infoString = "Total Students: " + studentCount
+            infoString = infoString + "  |  Fully Vaccinated: " + vaxComplete + "%"
+            infoString = infoString + "  |  One Dose: " + vaxFirst + "%"
+            if (studentCount != 0) {
+                schoolInfo.append("text")
+                .text(infoString)
+            }else{
+                schoolInfo.append("text")
+                .text("No Students Recorded")
+            }
             //Last Reported Cases
             var lastCaseReportedDate = "";
             var lastCaseReportedNumber = 0;
@@ -61,16 +134,16 @@ function getDailyTotals(schoolName) {
             };
             if (lastCaseReportedNumber == 1){
                 lastCaseReported.append("text")
-                .text("Last case (" + lastCaseReportedNumber + ") disclosed " + lastCaseReportedDate)
+                .text("Last case " + lastCaseReportedDate)
             } else if (lastCaseReportedNumber > 1) {
                 lastCaseReported.append("text")
-                .text("Last cases (" + lastCaseReportedNumber + ") disclosed " + lastCaseReportedDate)
+                .text("Last cases " + lastCaseReportedDate)
             } else {
                 lastCaseReported.append("text")
-                .text("0 cases have been disclosed")
+                .text("0 cases disclosed")
             }
             // 7 day total
-            var total7 = parseInt(data[3][schoolName], 10);
+            var total7 = parseInt(data[6][schoolName], 10);
             if (total7 == 1){
                 total7Day.append("text")
                 .text(total7 + " case in the last 7 days")
@@ -82,7 +155,7 @@ function getDailyTotals(schoolName) {
                 .text("0 cases in the last 7 days")
             }
             // 14 day total
-            var total14 = parseInt(data[4][schoolName], 10);
+            var total14 = parseInt(data[7][schoolName], 10);
             if (total14 == 1){
                 total14Day.append("text")
                 .text(total14 + " case in the last 14 days")
@@ -94,18 +167,18 @@ function getDailyTotals(schoolName) {
                 .text("0 cases in the last 14 days")
             }
 
-            // // 21 day total
-            // var total21 = parseInt(data[5][schoolName], 10);
-            // if (total21 == 1){
-            //     total21Day.append("text")
-            //     .text(total21 + " case in the last 21 days")
-            // } else if (total21 > 1) {
-            //     total21Day.append("text")
-            //     .text(total21 + " cases in the last 21 days")
-            // } else {
-            //     total21Day.append("text")
-            //     .text("0 cases in the last 21 days")
-            // }
+            // grand total
+            var grandTotal = total - syStartCase;
+            if (grandTotal == 1){
+                grandTotalCount.append("text")
+                .text(grandTotal + " case SY21-22")
+            } else if (grandTotal > 1) {
+                grandTotalCount.append("text")
+                .text(grandTotal + " cases SY21-22")
+            } else {
+                grandTotalCount.append("text")
+                .text("0 cases SY21-22")
+            }
             // Add Title
             totalCase.append("text")
                 .attr("x", (width / 2))             
@@ -176,7 +249,7 @@ function getDailyTotals(schoolName) {
                     .attr("dx", "-.8em")
                     .attr("dy", ".15em")
                     .attr("transform", "rotate(-65)");
-            // add X lable
+            // add X label
             dailyCase.append("text")             
                 .attr("transform", "translate(" + (dailyWidth/2) + " ," + (dailyHeight + dailyMargin.top + 20) + ")")
                 .style("text-anchor", "middle");
@@ -201,16 +274,28 @@ function getDailyTotals(schoolName) {
                 .text("Cases"); 
             
             // Daily case bars
+
             dailyCase.selectAll('.bar')
-                .data(dailyTotals)
-                .enter().append("rect")
-                .style("fill", "red")
-                .attr("class", "bar")
-                .attr("x", function(d) { return x(d.date); })
-                .attr("y", function(d) { return yLeft(d.cases); })
-                .attr("width", 1)
-                .attr("height", function(d) { return dailyHeight - yLeft(d.cases); });
+            .data(dailyTotals)
+            .enter().append("rect")
+            .style("fill", "red")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.date); })
+            .attr("y", function(d) { return yLeft(d.cases); })
+            // .attr("width", 3)
+            .attr("height", function(d) { return dailyHeight - yLeft(d.cases); });
+            if (caseSpan <= 60) {
+                dailyCase.selectAll('.bar')
+                    .attr("width", 5)
+            } else if (caseSpan <= 100) {
+                dailyCase.selectAll('.bar')
+                    .attr("width", 3)
+            } else {
+                dailyCase.selectAll('.bar')
+                    .attr("width", 1)
+            }
         }
     )
 };
-var schoolName = processSchool();
+var schoolName = getSchoolName();
+processSchool(schoolName);
